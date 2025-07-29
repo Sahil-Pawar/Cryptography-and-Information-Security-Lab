f63ffae1e38f7d7692ee68d1ff3d789a4d3b2043
Sahil Pawar <sahil9356926077@gmail.com>
2025-07-29 23:35:01 +0530

    First

---
+++ Assignement1/CeaserCiper.py
@@ -0,0 +1,32 @@
+# The code above implements a Caesar cipher for encryption and decryption.
+plaintext=input("Enter the plaintext:")
+key=int(input("Enter the key:"))
+
+def encrpyt(plaintext,key):
+    ciphertext = ""
+    for char in plaintext:
+        if char.isupper():
+            char=chr((ord(char)-ord('A')+key)%26+ord('A'))
+        elif char.islower():
+            char=chr((ord(char)-ord('a')+key)%26+ord('a'))
+        ciphertext += char
+    return ciphertext        
+
+def decrypt(ciphertext,key):
+    plaintext =""
+    for char in ciphertext:
+        if char.isupper():
+              char = chr(((ord(char) - ord('A') - key + 26) % 26) + ord('A'))
+        elif char.islower():
+              char = chr(((ord(char) - ord('a') - key + 26) % 26) + ord('a'))   
+        plaintext +=char
+    return plaintext
+
+ciphertext=encrpyt(plaintext,key)
+print("Ciphertext:",ciphertext)
+
+decrypted_text=decrypt(ciphertext,key)
+print("Decrypted text:", decrypted_text)
+
+
+
