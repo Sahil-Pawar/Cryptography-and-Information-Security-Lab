cdff663f4a84c249019634104cc92dfc75b541fb
Sahil Pawar <sahil9356926077@gmail.com>
2025-07-29 23:54:22 +0530

    first

---
+++ Assignement1/AtbashCipher.py
@@ -0,0 +1,16 @@
+# Atbash Cipher
+
+def atbash_cipher(plaintext):
+    ciphertext = ""
+    for char in plaintext:
+        if char.isupper():
+            ciphertext += chr(ord('A') + (ord('Z') - ord(char)))
+        elif char.islower():
+            ciphertext += chr(ord('a') + (ord('z') - ord(char)))
+        else:
+            ciphertext += char 
+    return ciphertext
+
+plaintext = input("Enter the plaintext: ")
+ciphertext = atbash_cipher(plaintext)
+print("Ciphertext:", ciphertext)
